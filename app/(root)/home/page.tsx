3f8ff9b43842eef2313dd61a427262e5f6e886bf
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-27 04:39:23 +0100

    Our story / about us section implemented

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import AuthProtection from "@/app/components/middleware/AuthProtection"
 import HeroSection from "@/app/components/ui/HeroSection";
-
-
+import Story from "@/app/components/ui/Story";
+import Memories from "@/app/components/ui/Memories";
 const Homepage = () => {
   return (
     <div>
       <HeroSection />
+      <Story />
+      <Memories />
     </div>
   )
 }
---
+++ app/components/ui/Memories.tsx
@@ -0,0 +1,12 @@
+const Memories = () => {
+    return (
+        <section className="bg-neutral-900 py-20">
+            <div className="max-w-6xl mx-auto text-white">
+                <h1 className="text-6xl font-semibold mb-3 text-orange-100">Our Special Memories So Far</h1>
+                <p className="text-gray-300">We've shared so many beautiful moments together. Here are some of our favorites.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Memories
