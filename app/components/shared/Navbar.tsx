6e9a08fdd99caf7be878a579aadee08854f56b2f
Somkene Ojukwu <somkeneoj@gmail.com>
2025-02-02 15:12:34 +0100

    Gallery url  added

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <nav className="hidden md:block lg:block bg-neutral-950 rounded-full text-pink-50 font-sans text-sm pr-3 pl-7 py-2 navbar">
                 <ul className="flex flex-row items-center gap-x-8 cursor-pointer">
                     {navigationOnLargeScreen.map((nav, index) => (
-                        <Link key={index} href={nav.href} {...(nav.name === 'Resume' && { target: "_blank", rel: "noopener noreferrer" })}>
+                        <Link key={index} href={nav.href} {...(nav.name === 'Gallery' && { target: "_blank", rel: "noopener noreferrer" })}>
                             <li className='hover:text-pink-300 transition duration-300 ease-in-out'>{nav.name}</li>
                         </Link>
                     ))}
@@ -30,7 +30,7 @@ const Navbar = () => {
             <nav className="md:hidden lg:hidden  bg-neutral-950 rounded-full text-pink-50 font-sans text-sm py-4 px-16 navbar nav-mobile">
                 <ul className="flex flex-row items-center gap-x-8 cursor-pointer">
                     {navigationOnMobile.map((mobileNav, index) => (
-                        <Link key={index} href={mobileNav.href} {...(mobileNav.name === 'Resume' && { target: "_blank", rel: "noopener noreferrer" })}>
+                        <Link key={index} href={mobileNav.href} {...(mobileNav.name === 'Gallery' && { target: "_blank", rel: "noopener noreferrer" })}>
                             <li className='hover:text-pink-300 transition duration-300 ease-in-out'>{mobileNav.name}</li>
                         </Link>
                     ))}
