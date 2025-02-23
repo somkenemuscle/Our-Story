0180d201de1e928bb8e1d9c899ae18954724874f
Somkene Ojukwu <somkeneoj@gmail.com>
2025-02-24 00:27:18 +0100

    Resume Updated

--- app/(root)/memories/page.tsx
+++ app/(root)/memories/page.tsx
@@ -3,7 +3,32 @@ import AuthProtection from "@/app/components/middleware/AuthProtection"
 
 const Memories = () => {
     return (
-        <div>Memories</div>
+        <div className="py-48 max-w-6xl mx-auto">
+            <div>
+                <h1 className="text-gray-500 font-semibold"><i>Happy 18th Birthday To My Wife</i> 💙</h1>
+                <h1 className="text-5xl font-bold">From your loving <i>Hubby</i> </h1>
+
+                <p className="mt-10">
+                    Happy Birthday, Baby! 🎉❤️
+                    Is there anything I’ll say now that I haven’t already told you every day? But still, Oko mi, I love you so much. Happy 18th birthday! Along with my mom, you are the most important woman in my life—my partner for life.
+                    We’ve been through so much together, mehnnn, fakkkk, but look at us now. We worked hard for our relationship, and we’re in a magnificent place. Now, let’s build financially as well. Let’s push each other closer to God.
+                </p>
+                <p className="mt-10">
+                    I pray that the start of your 18th year marks the beginning of something new and beautiful for both of us. I’ve also decided that I will turn a new leaf—we will start and succeed in everything we do. Consistency, always showing up, never missing time with God. When we feel tempted, we’ll talk, pray, and resist together. And when we get married? Well… unlimited sexual fun awaits. 😏❤️
+                </p>
+                <p className="mt-10">
+                    I pray for wisdom, knowledge, and understanding for you. Mehnnn, I love you so much, and I’m proud of the woman you’ve become—and the woman you are still growing to be. I want to always make you the happiest woman in the world, just as you make me the happiest man.
+                </p>
+                <p className="mt-10">
+                    God bless you and the day I laid my eyes on you. I’m proud of myself for telling you I liked you back then—best decision ever! There’s so much I could say, but words aren’t enough to describe my love for you. All I can do is keep showing it—by being there, helping you, assisting you, being patient, trusting you.
+                </p>
+                <p className="mt-10 font-semibold">
+                    Oko mi, my princess, my wife, my sweetheart, my gummy—mehnnn, muahhhh! 😘❤️ Happy 18th birthday, my beautiful, loving, caring woman.
+                    Welcome to adult life! 😂❤️❤️❤️
+
+                </p>
+            </div>
+        </div>
     )
 }
 
